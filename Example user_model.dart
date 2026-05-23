d3b3850883d9451dc09bb07c956fbc4df23a0523
186000percenta1-ctrl <186000percenta1@gmail.com>
2026-05-23 19:31:19 -0400

    Create Example user_model.dart

---
+++ Example user_model.dart
@@ -0,0 +1,21 @@
+class UserModel {
+  final String name;
+  final String bio;
+  final String location;
+  final String vibe;
+  final String age;
+  final String height;
+  final String relationshipStatus;
+  final String imageUrl;
+
+  UserModel({
+    required this.name,
+    required this.bio,
+    required this.location,
+    required this.vibe,
+    required this.age,
+    required this.height,
+    required this.relationshipStatus,
+    required this.imageUrl,
+  });
+}
